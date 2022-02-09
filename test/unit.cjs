d66cb6fd12ec949674413e6b0215f0495f223fe2
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-09 06:55:54 +0000

    TEST: Add tests

--- test/unit.cjs
+++ test/unit.cjs
@@ -1,19 +1,33 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const {convertRequireToImport} = require("../tools/converter.cjs");
+const {convertRequireToImport, stripComments} = require("../tools/converter.cjs");
 
 describe('converter.cjs', function() {
     describe('#stripComments()', function() {
-        it('should convert require cjs into esm export', function() {
+        it('should strip comments from strings', function() {
 
             // Arrange
-            const input = `const INFO1 = require("./dep-1.cjs");`
+            const input = `const cc = /* tttk k[gr */ \`var QuickLog3 = require("../../src/cjs/quick-log.cjs")\`;       // -------------
+// const aa = "let QuickLog2 = require(\\"../../src/cjs/quick-log.cjs\\")";`
 
             // Act
-            const result = convertRequireToImport(input)
+            const result = stripComments(input)
 
-            expect(result).to.contain('import INFO1  from "./dep-1.mjs";');
+            expect(result).to.equal('const cc =  `var QuickLog3 = require("../../src/cjs/quick-log.cjs")`;       \n');
+        });
+
+        it('should extract comments from strings', function() {
+
+            // Arrange
+            const input = `const cc = /* tttk k[gr */ \`var QuickLog3 = require("../../src/cjs/quick-log.cjs")\`;       // -------------
+// const aa = "let QuickLog2 = require(\\"../../src/cjs/quick-log.cjs\\")";`
+            const extracted = []
+
+            // Act
+            stripComments(input, extracted)
+
+            expect(extracted.length).to.equal(3);
         });
     });
 
