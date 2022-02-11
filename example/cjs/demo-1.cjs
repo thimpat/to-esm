ce368c451042aced7c454d5a8c0ee71034950ea3
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-11 19:48:24 +0000

    DEMO: Update examples

--- example/cjs/demo-1.cjs
+++ example/cjs/demo-1.cjs
@@ -3,11 +3,23 @@ const {LOG_CONTEXT, LOG_TARGETS} = require("./contexts-def.cjs")
 QuickLog = require("../../src/cjs/quick-log.cjs");
 let QuickLog2 = require("../../src/cjs/quick-log.cjs");
 var QuickLog3 = require("../../src/cjs/quick-log.cjs");
+var QuickLog4 = "rr";
+
 const cc = `var QuickLog3 = require("../../src/cjs/quick-log.cjs")`;
 // const aa = "let QuickLog2 = require(\"../../src/cjs/quick-log.cjs\")";
 
+QuickLog4 = require("../../src/cjs/quick-log.cjs")
+
+let myLog1, QuickLog5, yourLog;
+let myLog2, QuickLog6, yourLog2;
+let QuickLog7, myLog2, yourLog2;
+
 QuickLog.setContexts(LOG_CONTEXT);
 QuickLog.setTargets(LOG_TARGETS);
+
+QuickLog5 = require("../../src/cjs/quick-log.cjs");
+QuickLog6 = require("../../src/cjs/quick-log.cjs")
+QuickLog7 = require("../../src/cjs/quick-log.cjs")
 
 const {COLOR_TABLE, SYSTEM} = require("./constants.cjs");
 
