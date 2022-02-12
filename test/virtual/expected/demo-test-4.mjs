40a3ba55922b6b37c755ad9c76c70766e5af64b3
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-12 00:07:27 +0000

    DEMO: Update examples

---
+++ test/virtual/expected/demo-test-4.mjs
@@ -0,0 +1,11 @@
+/**
+ */
+const SPECIAL_CONSTANTS = {
+    WELCOME_CONTENT: "Welcome",
+};
+
+const SPECIAL_CONSTANTS_2 = {
+    COMMAND_NOT_SENT               : "COMMAND_NOT_SENT",
+};
+
+export default {SPECIAL_CONSTANTS, SPECIAL_CONSTANTS_2};
