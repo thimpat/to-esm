3f17c67fb3a2e4c18dc201beff455e224288ae5f
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-18 10:45:26 +0000

    TEST: Update tests

---
+++ test/assets/given/demo-test-12.cjs
@@ -0,0 +1,28 @@
+/** to-esm-browser: remove **/
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+/** to-esm-browser: end-remove **/
+
+abc/** to-esm-all: skip **/
+console.log("Skip this 1");
+/** to-esm-all: end-skip **/def
+
+const rgbHex = require("rgb-hex-cjs");
+const {COLOR_TABLE, SYSTEM} = require("./constants.cjs");
+
+const EOL =`
+`;
+
+ghi/** to-esm-all: skip **/
+console.log("Skip this 2");
+/** to-esm-all: end-skip **/jkl
+
+
+class Example
+{
+
+}
+
+module.exports = new Example();
+module.exports.anaLogger = new Example();
