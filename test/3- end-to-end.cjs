55e7f6957b49dd1d2fc6900552706c310ff24555
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-20 17:33:15 +0000

    TEST: Add end to end tests

---
+++ test/3- end-to-end.cjs
@@ -0,0 +1,37 @@
+const chai = require("chai");
+const shell = require("shelljs");
+const expect = chai.expect;
+const path = require("path");
+
+let rootDir = __dirname;
+
+describe("In the sandbox folder", function ()
+{
+    this.timeout(60000);
+
+    before(function ()
+    {
+        const assetsFolder = path.join(rootDir, "/sandbox");
+        process.chdir(assetsFolder);
+    });
+
+    describe("The sandbox project", function ()
+    {
+        it("should successfully install to-esm", function ()
+        {
+            const code = shell.exec("npm install ../../../to-esm").code;
+            expect(code).to.equal(0);
+        });
+
+        it("should successfully install AnaLogger", function ()
+        {
+            const code = shell.exec("npm install analogger").code;
+            expect(code).to.equal(0);
+        });
+
+        // ...
+
+    });
+
+
+});
