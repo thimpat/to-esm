44f601a44acf2e5ce91c553faf0f0723c6560c49
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-24 22:42:35 +0000

    FEAT: Display help with --help option

--- index.js
+++ index.js
@@ -4,12 +4,52 @@ const packageJson = require("./package.json");
 const minimist = require("minimist");
 const {convert} = require("./src/converter.cjs");
 
+const getHelp = () =>
+{
+    return `
+Usage:
+
+    to-esm <filepath> [--output <dirpath>] [--html <filepath>] [--noheader] [--target < browser|esm >] [--bundle <filepath>] [--update-all]
+
+Note:
+    🚫: to-esm should run from the project root folder.
+
+Examples:
+
+    - Generate an .mjs file from a .js file:
+    $> to-esm  example/cjs/input.js
+
+    - Generate code into a dedicated directory:
+    $> to-esm  example/cjs/input.cjs --output generated/esm
+
+    - Remove automatic generated header:
+    $> to-esm  example/cjs/input.cjs --output generated --noheader
+
+    - Generate code for the browser:
+    $> to-esm  example/cjs/input.cjs --output generated --target browser
+    
+    - Generate importmap after conversion:
+    $> to-esm example/cjs/demo.cjs --html index.html
+    
+    For more comprehensive help, go to: 
+    https://www.npmjs.com/package/to-esm
+    
+
+`;
+};
+
 (async () =>
 {
     const cliOptions = minimist(process.argv.slice(2));
     if (cliOptions.version || cliOptions.v)
     {
         console.log(`v${packageJson.version}`);
+        return;
+    }
+
+    if (cliOptions.help || cliOptions.h)
+    {
+        console.log(getHelp());
         return;
     }
     await convert(cliOptions);
