83e19c9b07d841aff3e61ac3ac042bfdfd6a997b
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-24 23:03:28 +0000

    TEST: Update tests

---
+++ test/2-integration-cli.cjs
@@ -0,0 +1,35 @@
+const chai = require("chai");
+const expect = chai.expect;
+const shell = require("shelljs");
+const packageJson = require("../package.json");
+
+describe("In the Terminal", function ()
+{
+    describe("to-esm", function ()
+    {
+        it("should display the version", function ()
+        {
+            let code = shell.exec("node ./index.js --version");
+            expect(code.stdout).to.contain(packageJson.version);
+        });
+
+        it("should display the version", function ()
+        {
+            let code = shell.exec("node ./index.js -v");
+            expect(code.stdout).to.contain(packageJson.version);
+        });
+
+        it("should display the help", function ()
+        {
+            let code = shell.exec("node ./index.js --help");
+            expect(code.stdout).to.contain("to-esm <filepath> [--output <dirpath>] [--html <filepath>] [--noheader] [--target < browser|esm >] [--bundle <filepath>] [--update-all]");
+        });
+
+        it("should display the help", function ()
+        {
+            let code = shell.exec("node ./index.js -h");
+            expect(code.stdout).to.contain("to-esm <filepath> [--output <dirpath>] [--html <filepath>] [--noheader] [--target < browser|esm >] [--bundle <filepath>] [--update-all]");
+        });
+
+    });
+});
