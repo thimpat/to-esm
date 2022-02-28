cd94558cce0b4c4e3b790518ccaea382d65c20e3
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-28 10:53:27 +0000

    TEST: Update tests

--- test/2-integration-cli.cjs
+++ test/2-integration-cli.cjs
@@ -19,13 +19,13 @@ describe("In the Terminal", function ()
             expect(code.stdout).to.contain(packageJson.version);
         });
 
-        it("should display the help", function ()
+        it("should display the help with --help option", function ()
         {
             let code = shell.exec("node ./index.js --help");
             expect(code.stdout).to.contain("to-esm <filepath> [--output <dirpath>] [--html <filepath>] [--noheader] [--target < browser|esm >] [--bundle <filepath>] [--update-all]");
         });
 
-        it("should display the help", function ()
+        it("should display the help with --h option", function ()
         {
             let code = shell.exec("node ./index.js -h");
             expect(code.stdout).to.contain("to-esm <filepath> [--output <dirpath>] [--html <filepath>] [--noheader] [--target < browser|esm >] [--bundle <filepath>] [--update-all]");
