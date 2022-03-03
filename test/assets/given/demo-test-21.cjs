ebffca4a79828ed599e7489f44e87d4e994100f5
Patrice THIMOTHEE <thimpat@gmail.com>
2022-03-03 09:22:49 +0000

    TEST: Update tests

---
+++ test/assets/given/demo-test-21.cjs
@@ -0,0 +1,72 @@
+/**
+ * Some code extract from module to use with test only
+ */
+'use strict';
+
+const { EMPTY_BUFFER } = require('./constants');
+
+function concat(list, totalLength) {
+}
+
+/**
+ * Some comment
+ *
+ */
+function _mask(source, mask, output, offset, length) {
+    for (let i = 0; i < length; i++) {
+        output[offset + i] = source[i] ^ mask[i & 3];
+    }
+}
+
+function _unmask(buffer, mask) {
+    for (let i = 0; i < buffer.length; i++) {
+        buffer[i] ^= mask[i & 3];
+    }
+}
+
+function toArrayBuffer(buf) {
+    if (buf.byteLength === buf.buffer.byteLength) {
+        return buf.buffer;
+    }
+
+    return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength);
+}
+
+/**
+ * Converts `data` to a `Buffer`.
+ *
+ * @param {*} data The data to convert
+ * @return {Buffer} The buffer
+ * @throws {TypeError}
+ * @public
+ */
+function toBuffer(data) {
+
+    return buf;
+}
+
+try {
+    const bufferUtil = require('bufferutil');
+
+    module.exports = {
+        concat,
+        mask(source, mask, output, offset, length) {
+            if (length < 48) _mask(source, mask, output, offset, length);
+            else bufferUtil.mask(source, mask, output, offset, length);
+        },
+        toArrayBuffer,
+        toBuffer,
+        unmask(buffer, mask) {
+            if (buffer.length < 32) _unmask(buffer, mask);
+            else bufferUtil.unmask(buffer, mask);
+        }
+    };
+} catch (e) /* istanbul ignore next */ {
+    module.exports = {
+        concat,
+        mask: _mask,
+        toArrayBuffer,
+        toBuffer,
+        unmask: _unmask
+    };
+}
