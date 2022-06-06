62cf11aedd23d1b4679ef7e951101715cd9f176d
Patrice Thimothee <thimpat@gmail.com>
2022-06-06 21:27:59 +0100

    DOCS: Update todos

--- todo.js
+++ todo.js
@@ -1,7 +1,5 @@
 
-// TODO: Cannot not minify ESM with native modules imports
-// TODO: Prevent .mjs conversion
-
 // Minor
 // TODO: Remove trailing semi-colon
 // TODO: Use forward slashes instead of back slashes
+// TODO: Allow generating bundle only (i.e. to-esm cjs/test-utils.cjs --target esm --bundle dist/test-utils.min.js)
