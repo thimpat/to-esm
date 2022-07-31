4eb3a86af78bafbad4203036a8c0d2a27c6f6b05
Patrice Thimothee <thimpat@gmail.com>
2022-07-31 08:39:45 +0100

    DEMO: Update todos

--- todo.js
+++ todo.js
@@ -2,9 +2,7 @@
 // TODO: Fix generated entrypoint file location
 // TODO: Minify .cjs
 // TODO: Add option --update-all-with-minified
+// TODO: Check why to-esm can't transform libutils with target browser
 //
 // Minor
-// TODO: Remove trailing semi-colon => See @test: should try to convert modules with non relative paths when target is
-//  browser
-// TODO: Use forward slashes instead of back slashes
-// TODO: Allow generating bundle only (i.e. to-esm cjs/test-utils.cjs --target esm --bundle dist/test-utils.min.js)
+//
