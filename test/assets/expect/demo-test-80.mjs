8c7de6d9b31cecd2659edef9b642bca86a9a4d92
Patrice Thimothee <thimpat@gmail.com>
2025-04-09 10:39:24 +0100

    test: Update tests

---
+++ test/assets/expect/demo-test-80.mjs
@@ -0,0 +1,38 @@
+import fs  from "fs";
+import path  from "path";
+import os  from "os";
+import ansi  from "ansi";
+import rgb  from "rgb";
+
+
+
+
+
+export const ff001  = function () {
+    try {
+        return true;
+    } catch (e) {
+        console.error({ lid: 1235 }, e.message);
+    }
+    return false;
+};
+export const add20  = function () {
+    try {
+        return true;
+    } catch (e) {
+        console.error({ lid: 1235 }, e.message);
+    }
+    return false;
+};
+export const add30  = function () {
+    try {
+        return true;
+    } catch (e) {
+        console.error({ lid: 1235 }, e.message);
+    }
+    return false;
+};
+
+
+
+const add10 = ff001;
