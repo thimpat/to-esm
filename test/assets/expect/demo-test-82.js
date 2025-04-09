a200d5fd789eab00516adef094a670ca63c47abe
Patrice Thimothee <thimpat@gmail.com>
2025-04-09 11:30:02 +0100

    test: Add tests for complex imports and extension handling

---
+++ test/assets/expect/demo-test-82.js
@@ -0,0 +1,12 @@
+import test2  from "./test2.js";
+
+test2.doIt();
+
+
+// test2.js
+
+export default {
+    doIt: () => {
+        console.log("hello")
+    }
+}
---
+++ test/assets/given/demo-test-81.cjs
@@ -0,0 +1,5 @@
+const {
+    AnyClass
+} = require('anylib');
+
+
